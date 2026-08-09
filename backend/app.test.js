570116a5f139242c5a0a7e90dc30c7a730e4dde7
unknown <Manando@Station1.hello.com>
2026-08-09 19:22:01 +0200

    Add backend.json and test for CI

---
+++ backend/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /api/hello', () => {
+    it('should return a JSON message', async () => {
+        const res = await request(app).get('/api/hello');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe('Hello from DevOps Lab Backend!');
+    });
+});
+
+describe('GET /api/stories', () => {
+    it('should return a list of stories', async () => {
+        const res = await request(app).get('/api/stories');
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBe(2);
+        expect(res.body[0]).toHaveProperty('title');
+        expect(res.body[0]).toHaveProperty('content');
+    });
+});
